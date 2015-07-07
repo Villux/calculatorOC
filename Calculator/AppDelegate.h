3a72178879f0ca4be8636072f33e9b3b6afc0b57
Ville Toiviainen <ville.toiviainen@futurice.com>
2015-07-07 18:15:21 +0300

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Ville Toiviainen on 07/07/15.
+//  Copyright (c) 2015 Ville Toiviainen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Ville Toiviainen on 07/07/15.
+//  Copyright (c) 2015 Ville Toiviainen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
